5db60d25aa9d6841cdaeb93ad91b1fc8f6b8204f
Yves <yspiri@fastmail.fm>
2015-05-08 23:19:04 +0200

    joghurt

--- save.php
+++ save.php
@@ -7,9 +7,11 @@ if (isset($_SESSION["logged_in"])){
 }
 
 $handle_benutzer_check = fopen ("benutzer.csv", "r");
-while($row = fgetcsv($handle_benutzer_check)) {
-		if( $row[0] == $userid AND $row[6] = "bewertet"){
-			exit;
+while ($row = fgetcsv($handle_benutzer_check)) {
+		if ($row[0] == $userid){
+			if ($row[6] == "bewertet"){
+				exit;
+			}
 		} else {
 			break;
 		}
@@ -25,8 +27,8 @@ fputcsv($handle_antworten, array(md5($userid), $fragen));
 $file = "benutzer.tmp";
 $handle_benutzer = fopen ("benutzer.csv", "r");
 $handle_benutzer_tmp = fopen ("benutzer.tmp", "w");
-while($row = fgetcsv($handle_benutzer)) {
-		if( $row[0] == $userid){
+while ($row = fgetcsv($handle_benutzer )) {
+		if ($row[0] == $userid){
 			$row[] = "bewertet";
 			$row[] = $score;
 		}
